711cea5fcf154c829f7cea9701735eeafd328560
lostpassword <gdb.lost@gmail.com>
2010-02-28 10:29:33 -0700

    new Class!

--- Admin/admin.js
+++ Admin/admin.js
@@ -33,9 +33,9 @@ admin.prototype = {
 		var content = "";
 		/* Login */
 		if ( this.login() ) {
-			divPagina.innerHTML = "Admin Control Panel<br><a href = '#admin' onClick = 'misTET.Admin.editMenu();'>Edita Menu</a><br>"+
+			divPagina.innerHTML = "Admin Control Panel<br><a href = '#admin' onClick = 'misTET.Admin.editMenu();'>Edita Menu</a><br"+
                                              "<a href = '#admin' onClick = 'misTET.Admin.editPagine();'>Edita Pagine</a><br>";
-                       divPagina.innerHTML += "<a href = '#' onClick = 'misTET.Admin.logout();'>Logout</a>";
+                      divPagina.innerHTML += "<a href = '#' onClick = 'misTET.Admin.logout();'>Logout</a>";
             	} else {
             		divPagina.innerHTML = "Login failed, control your password";
             	}
@@ -102,8 +102,8 @@ admin.prototype = {
 		if ( misTET.Admin.logged() ) {
 			var file = misTET.altro.importa('/res/files/menu.xml');
 			divPagina.innerHTML = "<form action = '/Admin/edit.php?menu&file=/res/files/menu.xml' method = 'POST'>" +
-                                             "<textarea name = 'newMenu' rows = '20' cols = '75'>"+file+"</textarea>" +
-                                             "<input type = 'submit' value = 'submit'></input>";
+                                            "<textarea name = 'newMenu' rows = '20' cols = '75'>"+file+"</textarea>" +
+                                            "<input type = 'submit' value = 'submit'></input>";
             } else {
             	divPagina.innerHTML = "Connessione...";
             	if ( misTET.Admin.login() ) {
@@ -119,8 +119,8 @@ admin.prototype = {
 		if ( misTET.Admin.logged() ) {
 			var file = misTET.altro.importa('/res/files/pagine.xml');
 			divPagina.innerHTML = "<form action = '/Admin/edit.php?pagine&file=/res/files/pagine.xml' method = 'POST'>" +
-                                             "<textarea name = 'newPage' rows = '20' cols = '75'>"+file+"</textarea>" +
-                                             "<input type = 'submit' value = 'submit'></input>";
+                                            "<textarea name = 'newPage' rows = '20' cols = '75'>"+file+"</textarea>" +
+                                            "<input type = 'submit' value = 'submit'></input>";
         } else {
         	divPagina.innerHTML = "Connessione...";
             if ( misTET.Admin.login() ) {
