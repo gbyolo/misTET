4729922fb032956839da089e04fa784337514770
lostpassword <gdb.lost@gmail.com>
2010-02-28 10:28:50 -0700

    Class.create()

--- Admin/admin.js
+++ Admin/admin.js
@@ -34,8 +34,8 @@ admin.prototype = {
 		/* Login */
 		if ( this.login() ) {
 			divPagina.innerHTML = "Admin Control Panel<br><a href = '#admin' onClick = 'misTET.Admin.editMenu();'>Edita Menu</a><br>"+
-                                              "<a href = '#admin' onClick = 'misTET.Admin.editPagine();'>Edita Pagine</a><br>";
-                        divPagina.innerHTML += "<a href = '#' onClick = 'misTET.Admin.logout();'>Logout</a>";
+                                             "<a href = '#admin' onClick = 'misTET.Admin.editPagine();'>Edita Pagine</a><br>";
+                       divPagina.innerHTML += "<a href = '#' onClick = 'misTET.Admin.logout();'>Logout</a>";
             	} else {
             		divPagina.innerHTML = "Login failed, control your password";
             	}
@@ -62,7 +62,7 @@ admin.prototype = {
 			onFailure: function (http) {
 				error 			= new Error("Impossibile ricevere il file #{status}".interpolate(http));
 				error.name 		= "receiveError";
-				error.fileName  = path;
+				error.fileName          = path;
 			}
 		});
 		
@@ -102,8 +102,8 @@ admin.prototype = {
 		if ( misTET.Admin.logged() ) {
 			var file = misTET.altro.importa('/res/files/menu.xml');
 			divPagina.innerHTML = "<form action = '/Admin/edit.php?menu&file=/res/files/menu.xml' method = 'POST'>" +
-                                              "<textarea name = 'newMenu' rows = '20' cols = '75'>"+file+"</textarea>" +
-                                              "<input type = 'submit' value = 'submit'></input>";
+                                             "<textarea name = 'newMenu' rows = '20' cols = '75'>"+file+"</textarea>" +
+                                             "<input type = 'submit' value = 'submit'></input>";
             } else {
             	divPagina.innerHTML = "Connessione...";
             	if ( misTET.Admin.login() ) {
@@ -119,12 +119,12 @@ admin.prototype = {
 		if ( misTET.Admin.logged() ) {
 			var file = misTET.altro.importa('/res/files/pagine.xml');
 			divPagina.innerHTML = "<form action = '/Admin/edit.php?pagine&file=/res/files/pagine.xml' method = 'POST'>" +
-                                              "<textarea name = 'newPage' rows = '20' cols = '75'>"+file+"</textarea>" +
-                                              "<input type = 'submit' value = 'submit'></input>";
+                                             "<textarea name = 'newPage' rows = '20' cols = '75'>"+file+"</textarea>" +
+                                             "<input type = 'submit' value = 'submit'></input>";
         } else {
         	divPagina.innerHTML = "Connessione...";
             if ( misTET.Admin.login() ) {
-            	misTET.Admin.editMenu();
+            	 misTET.Admin.editMenu();
             } else {
             	divPagina.innerHTML = "Impossibile effettuare il login, controlla i dati";
             }
