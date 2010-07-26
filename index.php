c4b6f55a1c3b1f58006c57205dc76d68ec40564e
lostpassword <gdb.lost@gmail.com>
2010-07-26 17:53:59 +0200

    new theme

--- index.php
+++ index.php
@@ -32,43 +32,26 @@
 
 		<link rel = "stylesheet" href = "/modules/sintax/styles/shCore.css" type = "text/css">
 		<link rel = "stylesheet" href = "/modules/sintax/styles/shThemeEclipse.css" type = "text/css" >
-		<link rel = "stylesheet" href = "/res/styles/style.css" type = "text/css">
+		<link rel = "stylesheet" href = "/res/styles/light.css" type = "text/css">
 		<link rel = "stylesheet" href = "/res/styles/menu.css" type = "text/css">
 
-		<title>misTET - Power on earth</title>
+		<title></title>
 	</head>
 <body onLoad = "misTET.init();">
 
-	<div id="content">
-			<div id = 'preheader'></div>
-			<div id = "header">
-				<div id = "title">misTET - Power on earth</div>
-				<div id = "slogan">Your best cms.</div>
-			</div>
+	<div id = "title"></div>
 			
-			<div id = "menu">
-			</div>
+	<div id = "menu">
+	</div>
 			
-			<div id = "main_content">
-				<div id = "sd_right">
-					<div id = "text_padding">
-						<h2>Info</h2>
-						e-mail: <a href = "mailto:gdb.lost@gmail.com">gdb.lost[at]gmail[dot]com</a>
-						<a href = "http://github.com/lostpassword/misTET" target = "_blank">Github</a>
-					</div>
-				</div>
-				
-				<div id = "sd_left">
-				</div>
+	<div id = "page">
+	</div>
 			
-				<div id = "footer">
-					<div id = "padding">
-                    &copy; 2010 <a href = ""><strong>misTET</strong></a>.
-
-| <a href = "rss/">RSS Feed</a> | <a href = "javascript:misTET.check.css(window.location.href);">CSS</a> and <a href = "javascript:misTET.check.html(window.location.href);">HTML</a>  |
-					</div>
-				</div>
-			</div>
+	<div class = "footer">
+		<div id = "padding">
+            &copy; 2010 <a href = ""><strong>misTET</strong></a>.
+			| <a href = "rss/">RSS Feed</a> | <a href = "javascript:misTET.check.css(window.location.href);">CSS</a> and <a href = "javascript:misTET.check.html(window.location.href);">HTML</a>  |
+		</div>
 	</div>
 	
 </body>
