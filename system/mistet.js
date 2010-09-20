9d6d3c54a00b09fe2774b73dc1152095997539dc
lostpassword <gdb.lost@gmail.com>
2010-09-20 17:11:47 +0200

    I hate git

--- system/mistet.js
+++ system/mistet.js
@@ -1,22 +1,22 @@
-/****************************************************************************
- * Copyleft lostpassword                                                    *
+/**************************************************************************** 
+ * Copyleft lostpassword                                                    * 
  * [gdb.lost@gmail.com]                                                     *
  *                                                                          *
+ * This file is part of misTET.                                             *
  *                                                                          *
  * misTET is free software: you can redistribute it and/or modify           *
- * it under the terms of the GNU General Public License as published by     *
- * the Free Software Foundation, either version 3 of the License, or        *
- * (at your option) any later version.                                      *
+ * it under the terms of the GNU Affero General Public License as           *
+ * published by the Free Software Foundation, either version 3 of the       *
+ * License, or (at your option) any later version.                          *
  *                                                                          *
  * misTET is distributed in the hope that it will be useful,                *
  * but WITHOUT ANY WARRANTY; without even the implied warranty of           *
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
- * GNU General Public License for more details.                             *
+ * GNU Affero General Public License for more details.                      *
  *                                                                          *
- * You should have received a copy of the GNU General Public License        *
+ * You should have received a copy of the GNU Affero General Public License *
  * along with misTET.  If not, see <http://www.gnu.org/licenses/>.          *
  ****************************************************************************/
-
 
 var misTET = {
     
@@ -568,32 +568,32 @@ var misTET = {
             return true;
                     
         },
-		
-		get: function (name) {
-		
-			if (!Object.isset(name)) {
-				misTET.errors.create({
-					name: "misTET.modules.get",
-					message: "0 of 1 arguments"
-				});
-				return false;
-			}
-			
-			if (!Object.isString(name)) {
-				misTET.errors.create({
-					name: "misTET.modules.get",
-					message: "wrong type of argument"
-				});
-				return false;
-			}
-			
-			if (misTET.modules.exists(name)) {
-				return misTET.modules[name];
-			} else {
-				return false;
-			}
-			
-		},
+                
+        get: function (name) {
+                
+            if (!Object.isset(name)) {
+                misTET.errors.create({
+                    name: "misTET.modules.get",
+                    message: "0 of 1 arguments"
+                });
+                return false;
+            }
+                        
+            if (!Object.isString(name)) {
+                misTET.errors.create({
+                    name: "misTET.modules.get",
+                    message: "wrong type of argument"
+                });
+                return false;
+            }
+                        
+            if (misTET.modules.exists(name)) {
+                return misTET.modules[name];
+            } else {
+                return false;
+            }
+                        
+        },
             
         exists: function (name) {
             if (!name) {
@@ -1152,4 +1152,4 @@ var misTET = {
     }
 };
 
-misTET.utils.include("system/utils.js");
+misTET.utils.include("system/framework.js");
