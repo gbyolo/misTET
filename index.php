b461f8df8572dd7ffcb1cfe31f42e39dec5697cc
lostpassword <gdb.lost@gmail.com>
2011-03-20 23:21:10 +0100

    [STATIC] a lot of work

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+/**************************************************************************** 
+ * Copyleft lostpassword                                                    * 
+ * [gdb.lost@gmail.com]                                                     *
+ *                                                                          *
+ * This file is part of misTET.                                             *
+ *                                                                          *
+ * misTET is free software: you can redistribute it and/or modify           *
+ * it under the terms of the GNU Affero General Public License as           *
+ * published by the Free Software Foundation, either version 3 of the       *
+ * License, or (at your option) any later version.                          *
+ *                                                                          *
+ * misTET is distributed in the hope that it will be useful,                *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ * GNU Affero General Public License for more details.                      *
+ *                                                                          *
+ * You should have received a copy of the GNU Affero General Public License *
+ * along with misTET.  If not, see <http://www.gnu.org/licenses/>.          *
+ ****************************************************************************/
+
+/* version */
+define('__VERSION__', '0.1');
+/* root path */
+define('__ROOT__', basename(dirname(__FILE__)));
+/* extern files folder */
+define('EXTERN', __ROOT__.'/stat');
+/* modules folder */
+define('MODULES', __ROOT__.'/modules');
+
+/* require core */
+require(__ROOT__.'/system/misTET.php')
+
+/* Have to write all */
+
+?>
