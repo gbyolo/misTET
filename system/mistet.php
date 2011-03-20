46f985586d87e1bf64885e59c3afc7875daeb69d
lostpassword <gdb.lost@gmail.com>
2011-03-20 23:38:30 +0100

    [Static] developing core

---
+++ system/mistet.php
@@ -0,0 +1,37 @@
+<?php
+/**************************************************************************** 
+ * Copyleft lostpassword                                                    * 
+ * [gdb.lost@gmail.com]                                                     *
+ *                                                                          *
+ * This file is part of misTET.                                             *
+ *                                                                          *
+ * misTET is free software: you can redistribute it and/or modify           *
+ * it under the terms of the GNU Affero General Public License as           *
+ * published by the Free Software Foundation, either version 3 of the       *
+ * License, or (at your option) any later version.                          *
+ *                                                                          *
+ * misTET is distributed in the hope that it will be useful,                *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ * GNU Affero General Public License for more details.                      *
+ *                                                                          *
+ * You should have received a copy of the GNU Affero General Public License *
+ * along with misTET.  If not, see <http://www.gnu.org/licenses/>.          *
+ ****************************************************************************/
+ 
+ class misTET
+ {
+	 public $version = '0.1';
+	 
+	 private $config = array("initialized" => false,
+							"home" => "",
+							"title" => "",
+							"loading" => "",
+							);
+	
+	public function __construct () {
+		/* initialize */
+	}
+ }
+ 
+ ?>
