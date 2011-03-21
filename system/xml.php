f806a1a7d9afdf546030992612429ca05285676b
lostpassword <gdb.lost@gmail.com>
2011-03-21 19:21:43 +0100

    Starting to write XMLparser

---
+++ system/xml.php
@@ -0,0 +1,38 @@
+<?php
+/**************************************************************************** 
+ * Copyleft lostpassword                                                    * 
+ * [gdb.lost@gmail.com]                                                     *
+ *                                                                          *
+ * This file is part of misTET.                                             *
+ *                                                                          *
+ * misTET is free software: you can redistribute it and/or modify           *
+ * it under the terms of the GNU Affero General Public License as           *
+ * published by the Free Software Foundation, either version 3 of the       *
+ * License, or (at your option) any later version.                          *
+ *                                                                          *
+ * misTET is distributed in the hope that it will be useful,                *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *
+ * GNU Affero General Public License for more details.                      *
+ *                                                                          *
+ * You should have received a copy of the GNU Affero General Public License *
+ * along with misTET.  If not, see <http://www.gnu.org/licenses/>.          *
+ ****************************************************************************/
+ 
+ class XMLparser
+ {
+	
+	public function init ($file) {
+		
+		$xml = simplexml_load_file($file);
+		
+		return array(
+			"home" => (string) $xml->homePage,
+			"title" => (string) $xml->title
+		);
+		
+	} 
+	 
+ }
+ 
+ ?>
